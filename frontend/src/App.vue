db7b67e1ebcfbdfbfee186f276651424e8fc01a9
Anna de Azevedo Maciel <annadeazevedo@Poyo663.poyo>
2025-11-24 21:01:51 -0300

    Adicionei um componente de formulario generico, junto com instrucoes pra usar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,28 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { ref } from 'vue'
+import Cadastro from './components/Cadastro.vue'
+
+const props = ref([
+  {
+    name: 'email',
+    input: 'email',
+    placeholder: 'E-mail',
+  },
+  {
+    name: 'senha',
+    input: 'password',
+  },
+  {
+  name: 'confirmar senhar',
+  input: 'password',
+  }
+])
 </script>
 
 <template>
-  <RouterView />
+  <!-- <RouterView /> -->
+  <Cadastro title="Tema" :options="props" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ frontend/src/components/Cadastro.vue
@@ -0,0 +1,28 @@
+<!-- Opcoes para cada elemento do form  -->
+<!--   name -> nome do campo, vai fazer display emcima do elemento, tbm séra o nome no elemento  -->
+<!--   input -> o tipo do input html que será usado -->
+<!--   placeholder -> o texto que vai aparecer se usar algum campo de texto -->
+<!--   subtitle -> subtitulo que vai aparecer do lado do elemento (feito para inputs do tipo checkbox) -->
+
+<script setup>
+const props = defineProps({
+  title: String,
+  options: [Object],
+})
+</script>
+
+<template>
+  <h1>{{ props.title }}</h1>
+  <form action="get">
+    <div v-for="option in props.options">
+      <h3>{{ option.name }}</h3>
+      <input
+        :name="option.name"
+        :type="option.input"
+        :placeholder="option.placeholder"
+      />
+      <span v-if="option.subtitle">{{ option.subtitle }}</span>
+    </div>
+    <input type="submit" />
+  </form>
+</template>
