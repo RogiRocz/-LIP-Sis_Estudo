3cbf9954cef510f579dcb43d641993c9878e8bfc
rogirocz2 <rogirocz@gmail.com>
2025-12-09 14:38:57 +0000

    feat: adicionando dependencias e configurações para rodar o eslint e prettier

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,13 +1,14 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
 }
