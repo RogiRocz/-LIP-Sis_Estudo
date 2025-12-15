466f60549c483940297c82e8279f89bf52735924
rogirocz2 <rogirocz@gmail.com>
2025-12-15 01:44:50 +0000

    feat: adição da prop routeName

--- frontend/src/utils/tabsNavigation.ts
+++ frontend/src/utils/tabsNavigation.ts
@@ -1,24 +1,28 @@
 import { ref } from 'vue'
 
 export const tabsNavigation = ref([
-  {
-    name: 'Disciplinas',
-    iconName: 'book',
-    componentName: 'DisciplinasComponent',
-  },
-  {
-    name: 'Cronograma',
-    iconName: 'calendar_month',
-    componentName: 'CronogramaComponent',
-  },
-  {
-    name: 'Relatórios',
-    iconName: 'bar_chart',
-    componentName: 'RelatoriosComponent',
-  },
-  {
-    name: 'Configurações',
-    iconName: 'settings',
-    componentName: 'ConfiguracoesComponent',
-  },
+	{
+		name: 'Disciplinas',
+		iconName: 'book',
+		componentName: 'DisciplinasComponent',
+		routeName: 'disciplina',
+	},
+	{
+		name: 'Cronograma',
+		iconName: 'calendar_month',
+		componentName: 'CronogramaComponent',
+		routeName: 'calendario',
+	},
+	{
+		name: 'Relatórios',
+		iconName: 'bar_chart',
+		componentName: 'RelatoriosComponent',
+		routeName: 'relatorios',
+	},
+	{
+		name: 'Configurações',
+		iconName: 'settings',
+		componentName: 'ConfiguracoesComponent',
+		routeName: 'configuracoes',
+	},
 ])
