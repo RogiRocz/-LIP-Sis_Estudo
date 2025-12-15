4067d86636285a11d4f8834af3c0e0ed4a907007
rogirocz2 <rogirocz@gmail.com>
2025-12-15 01:44:11 +0000

    feat: adicionando rotas iniciais do app

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/useUserStore'
+import { storeToRefs } from 'pinia'
+
+const routes = [
+	{ path: '/', name: 'home', component: import('@/views/HomeView.vue') },
+	{ path: '/login', name: 'login', component: import('@/views/LoginView.vue') },
+	{
+		path: '/disciplina',
+		name: 'disciplina',
+		component: import('@/views/DisciplinaView.vue'),
+	},
+	{
+		path: '/calendario',
+		name: 'calendario',
+		component: import('@/views/CalendarioView.vue'),
+	},
+	{
+		path: '/relatorios',
+		name: 'relatorios',
+		component: import('@/views/RelatoriosView.vue'),
+	},
+	{
+		path: '/configuracoes',
+		name: 'configuracoes',
+		component: import('@/views/ConfiguracoesView.vue'),
+	},
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+})
+
+router.beforeEach((to, from) => {
+	const userStore = useUserStore()
+	const { token } = storeToRefs(userStore)
+
+	const publicRoutes = ['login']
+
+	// if (!publicRoutes.includes(to.name) && !token.value) {
+	// 	return { name: 'login' }
+	// }
 })
 
 export default router
