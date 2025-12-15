f84c955f9ec0ae096f93cdddba63410bb9a7a50a
rogirocz2 <rogirocz@gmail.com>
2025-12-15 04:21:56 +0000

    feat: criando o icone de navegação

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 import AppBar from './components/AppBar.vue'
+import { tabsNavigation } from './utils/tabsNavigation'
+import { useAppBarStore } from './stores/useAppBarStore'
+import { storeToRefs } from 'pinia'
+
+const appBarStore = useAppBarStore()
+const { drawer } = storeToRefs(appBarStore)
 </script>
 
 <template>
 	<v-app>
+		<v-navigation-drawer temporary v-model="drawer">
+			<v-list-item-title id="titulo">
+				<v-icon icon="book_ribbon" class="mr-2"></v-icon>
+				StudyFlow
+			</v-list-item-title>
+			<v-divider></v-divider>
+			<v-list-item
+				v-for="(tab, i) in tabsNavigation"
+				:key="i"
+				:prepend-icon="tab.iconName"
+				:title="tab.name"
+				:to="{ name: tab.routeName }"
+			></v-list-item>
+		</v-navigation-drawer>
 		<AppBar />
 		<v-main>
 			<router-view v-slot="{ Component }">
@@ -15,4 +35,18 @@ import AppBar from './components/AppBar.vue'
 	</v-app>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-main {
+	margin: 1vh 1vw;
+}
+
+.v-list-item-title {
+	line-height: 3.5;
+}
+
+#titulo {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
