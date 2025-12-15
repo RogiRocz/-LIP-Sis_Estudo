237390c158922e3112340a32b210389bdfff7011
rogirocz2 <rogirocz@gmail.com>
2025-12-15 01:43:18 +0000

    refat: tirando ;

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
-import "./assets/main.css";
-import vuetify from "./config/vuetify";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import './assets/main.css'
+import vuetify from './config/vuetify'
 
-const app = createApp(App);
-app.use(createPinia());
-app.use(vuetify);
-app.use(router);
-app.mount("#app");
+const app = createApp(App)
+app.use(createPinia())
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
