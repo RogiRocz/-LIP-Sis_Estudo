ec488fed4b4a51f706166fae40fd7d90cd8a0655
rogirocz2 <rogirocz@gmail.com>
2025-12-16 00:02:25 +0000

    feat: criando espaço de titulo e descricao das paginas

---
+++ frontend/src/components/PageHeader.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-container no-gutters fluid>
+		<v-row id="page-header">
+			<v-col id="page-header-info">
+				<h2>{{ pageTitle }}</h2>
+				<p>{{ pageDescription }}</p>
+			</v-col>
+			<slot id="page-header-actions"> </slot>
+		</v-row>
+	</v-container>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	pageTitle: string,
+	pageDescription: string,
+}>();
+</script>
+
+<style scoped>
+h2 {
+	font-weight: 400;
+}
+
+p {
+	color: #64748b;
+}
+
+</style>
