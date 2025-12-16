df7ab46beb9cb789fc74b04ac087f3dc79617bd7
rogirocz2 <rogirocz@gmail.com>
2025-12-16 00:03:17 +0000

    feat: criando prop description para funcionar no pageHeader

--- frontend/src/utils/tabsNavigation.ts
+++ frontend/src/utils/tabsNavigation.ts
@@ -6,23 +6,27 @@ export const tabsNavigation = ref([
 		iconName: 'book',
 		componentName: 'DisciplinasComponent',
 		routeName: 'disciplina',
+		description: 'Organize seus estudos por disciplinas e temas',
 	},
 	{
 		name: 'Cronograma',
 		iconName: 'calendar_month',
 		componentName: 'CronogramaComponent',
 		routeName: 'calendario',
+		description: 'Acompanhe suas revisões agendadas e sessões de estudo',
 	},
 	{
 		name: 'Relatórios',
 		iconName: 'bar_chart',
 		componentName: 'RelatoriosComponent',
 		routeName: 'relatorios',
+		description: 'Acompanhe seu desempenho e evolução',
 	},
 	{
 		name: 'Configurações',
 		iconName: 'settings',
 		componentName: 'ConfiguracoesComponent',
 		routeName: 'configuracoes',
+		description: 'Ajustes e preferências do aplicativo.',
 	},
 ])
