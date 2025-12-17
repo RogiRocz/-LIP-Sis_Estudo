afeea9e6e2c36f7f6f4277611fcfc91721f05920
rogirocz2 <rogirocz@gmail.com>
2025-12-17 01:40:40 +0000

    refat: ajuste na forma de autenticação para funcionarn o swagger

--- backend/src/security.py
+++ backend/src/security.py
@@ -2,15 +2,16 @@ from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 from typing import Optional
-from core.config import settings
+from .core.config import settings
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
-from models.user import User
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from .models.user import User
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from database import get_db
+from .database import get_db
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+security_scheme = HTTPBearer()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -30,13 +31,14 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET, algorithm="HS256")
     return encoded_jwt
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security_scheme), db: AsyncSession = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
+        token = credentials.credentials
         payload = jwt.decode(token, settings.JWT_SECRET, algorithms=["HS256"])
         email: str = payload.get("sub")
         if email is None:
