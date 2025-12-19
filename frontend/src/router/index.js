1fc1bbab160489b6daf5d00b89fc93ddbed6fbdb
rogirocz2 <rogirocz@gmail.com>
2025-12-19 00:18:43 +0000

    refat: aplicando import dinâmico e verificação autenticidade de rotas

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,42 +2,42 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useUserStore } from '@/stores/useUserStore'
 import { storeToRefs } from 'pinia'
 
+
 const routes = [
 	{
 		path: '/',
 		name: 'home',
-		component: import('@/views/HomeView.vue'),
-		redirect: { name: 'cadastro' },
+		component: () => import('@/views/HomeView.vue'),
 	},
 	{
 		path: '/login',
 		name: 'login',
-		component: import('@/views/LoginView.vue'),
+		component: () => import('@/views/LoginView.vue'),
 	},
 	{
 		path: '/cadastro',
 		name: 'cadastro',
-		component: import('@/views/CadastroView.vue'),
+		component: () => import('@/views/CadastroView.vue'),
 	},
 	{
 		path: '/disciplina',
 		name: 'disciplina',
-		component: import('@/views/DisciplinaView.vue'),
+		component: () => import('@/views/DisciplinaView.vue'),
 	},
 	{
 		path: '/calendario',
 		name: 'calendario',
-		component: import('@/views/CalendarioView.vue'),
+		component: () => import('@/views/CalendarioView.vue'),
 	},
 	{
 		path: '/relatorios',
 		name: 'relatorios',
-		component: import('@/views/RelatoriosView.vue'),
+		component: () => import('@/views/RelatoriosView.vue'),
 	},
 	{
 		path: '/configuracoes',
 		name: 'configuracoes',
-		component: import('@/views/ConfiguracoesView.vue'),
+		component: () => import('@/views/ConfiguracoesView.vue'),
 	},
 ]
 
@@ -46,15 +46,18 @@ const router = createRouter({
 	routes,
 })
 
-router.beforeEach((to, from) => {
+router.beforeEach((to, from, next) => {
 	const userStore = useUserStore()
-	const { token } = storeToRefs(userStore)
+	const { isAuthenticated } = storeToRefs(userStore)
 
-	const publicRoutes = ['login']
+	const publicRoutes = ['login', 'cadastro']
+	const authRequired = !publicRoutes.includes(to.name)	
 
-	// if (!publicRoutes.includes(to.name) && !token.value) {
-	// 	return { name: 'login' }
-	// }
+	if (authRequired && !isAuthenticated.value) {
+		next({ name: 'login' })
+	} else {
+		next()
+	}
 })
 
 export default router
