577468fd677d4aa7737389bf1560fcebadca05de
rogirocz2 <rogirocz@gmail.com>
2025-12-19 00:11:33 +0000

    feat: ajustando para problema de CORS e definindo porta

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import auth, user, disciplina, tema, revisao
 from .database import Base, engine
 from sqlalchemy import text
@@ -34,6 +35,14 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(disciplina.router)
@@ -44,3 +53,8 @@ app.include_router(revisao.router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+if __name__ == "__main__":
+    # Definindo a porta e o host para o servidor
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
