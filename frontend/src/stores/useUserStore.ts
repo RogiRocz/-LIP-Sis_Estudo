3f31436343d940be31b8f31b03100be0ab6c79f2
rogirocz2 <rogirocz@gmail.com>
2025-12-19 00:19:35 +0000

    feat: criando lógica para salva token e usuário na store

--- frontend/src/stores/useUserStore.ts
+++ frontend/src/stores/useUserStore.ts
@@ -1,10 +1,26 @@
+import { Usuario } from '@/utils/apiTypes'
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { getProfile } from '@/api/user'
 
-export const useUserStore = defineStore('user', () => {
-	const token = ref<String | null>(localStorage.getItem('authToken'))
+export const useUserStore = defineStore('userStore', () => {
+	// State
+	const drawerAuth = ref(false)
+	const loading = ref(false)
+	const token = ref(localStorage.getItem('authToken') || null)
+	const user = ref<Usuario | null>(null)
 
-	function setToken(newToken) {
+	// Getters
+	const isDrawerOpen = computed(() => drawerAuth.value)
+	const isAuthenticated = computed(() => !!token.value)
+	const getUser = computed(() => user.value)
+
+	// Actions
+	const toggleDrawerAuth = () => {
+		drawerAuth.value = !drawerAuth.value
+	}
+
+	const setToken = (newToken: string | null) => {
 		token.value = newToken
 		if (newToken) {
 			localStorage.setItem('authToken', newToken)
@@ -13,17 +29,47 @@ export const useUserStore = defineStore('user', () => {
 		}
 	}
 
-	function clearToken() {
-		token.value = null
-		localStorage.removeItem('authToken')
+	const setUser = (newUser: Usuario | null) => {
+		if (newUser) {
+			user.value = newUser
+		} else {
+			user.value = null
+		}
 	}
 
-	const isAuthenticated = () => !!token.value
+	const logout = () => {
+		localStorage.removeItem('authToken')
+		token.value = null
+		user.value = null
+	}
+
+	const fetchUser = async () => {
+		if (token.value) {
+			try {
+				const userData = await getProfile()
+				setUser(userData)
+			} catch (error) {
+				console.error(
+					'Falha ao buscar usuário. Token pode ser inválido.',
+					error,
+				)
+				logout()
+			}
+		}
+	}
 
 	return {
+		drawerAuth,
+		loading,
 		token,
+		user,
 		setToken,
-		clearToken,
+		setUser,
 		isAuthenticated,
+		getUser,
+		isDrawerOpen,
+		toggleDrawerAuth,
+		logout,
+		fetchUser,
 	}
 })
