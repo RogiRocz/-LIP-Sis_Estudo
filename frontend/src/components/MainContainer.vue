c5771880637516fe4fa7fa5adf4cad221b199e44
rogirocz2 <rogirocz@gmail.com>
2025-12-23 01:25:23 +0000

    feat: component geral que fica o conteudo principal das tabs

---
+++ frontend/src/components/MainContainer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<v-container fluid fill-height>
+		<v-col>
+			<slot name="main-content"></slot>
+		</v-col>
+	</v-container>
+</template>
+
+<style scoped>
+</style>
