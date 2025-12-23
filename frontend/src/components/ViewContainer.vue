0aa9812f88ae5cdaf725686d5e24c53759a92a64
rogirocz2 <rogirocz@gmail.com>
2025-12-23 20:41:57 +0000

    feat: criando wrapper para as views

---
+++ frontend/src/components/ViewContainer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+	<v-container fluid class="pa-0 ma-0">
+		<slot name="view-content"></slot>
+	</v-container>
+</template>
+
+<style scoped>
+.v-container {
+    height: 100%;
+}
+</style>
