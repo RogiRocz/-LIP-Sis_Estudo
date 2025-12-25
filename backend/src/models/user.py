ef1685523f6d790048e71cf4eef2952e3d92833a
rogirocz2 <rogirocz@gmail.com>
2025-12-25 04:20:02 +0000

    feat: adição do supabase_id para ativar o RLS

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from ..database import Base
 from .base import TimestampMixin
+from sqlalchemy.dialects.postgresql import UUID
+
 
 class User(Base, TimestampMixin):
     __tablename__ = 'Usuario'
@@ -11,5 +13,6 @@ class User(Base, TimestampMixin):
     senha = Column(String(100), nullable=False)
     ui_theme = Column(String(10), default='light')
     intervalo_revisoes = Column(String(50), default='1,7,14')
+    supabase_id = Column(UUID(as_uuid=True), unique=True, nullable=True)
         
     disciplinas = relationship("Disciplina", back_populates="usuario")
