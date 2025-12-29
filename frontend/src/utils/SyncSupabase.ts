76089ed07a0ec0d09c5b2b1b70094b2753f8c8d9
rogirocz2 <rogirocz@gmail.com>
2025-12-29 18:16:20 +0000

    refat: melhorias na lógica

--- frontend/src/utils/SyncSupabase.ts
+++ frontend/src/utils/SyncSupabase.ts
@@ -2,29 +2,34 @@ const syncArray = (currentArray: any[] | null, p: any) => {
 	if (!currentArray) return []
 
 	const { eventType, new: newItem, old: oldItem } = p
-
+    
 	if (eventType === 'INSERT') return [...currentArray, newItem]
 	if (eventType === 'UPDATE') return currentArray.map((item) => (item.ID === newItem.ID ? newItem : item))
 	if (eventType === 'DELETE') return currentArray.filter((item) => item.ID !== oldItem.ID)
+
+    
 
 	return currentArray
 }
 
 const syncMap = (currentMap: Map<number, any[]> | null, payload: any, parentKeyName: string) => {
     if (!currentMap) return new Map();
-    const { eventType, new: newItem, old: oldItem } = payload;
     
+    const { eventType, new: newItem, old: oldItem } = payload;
+    if (!newItem && eventType !== 'DELETE') return currentMap;
+
     const parentId = eventType === 'DELETE' ? oldItem[parentKeyName] : newItem[parentKeyName];
-    const list = currentMap.get(parentId) || [];
-  
+    const list = [...(currentMap.get(parentId) || [])];
+
     let newList: any[];
     if (eventType === 'INSERT') newList = [...list, newItem];
     else if (eventType === 'UPDATE') newList = list.map(item => item.ID === newItem.ID ? newItem : item);
     else if (eventType === 'DELETE') newList = list.filter(item => item.ID !== oldItem.ID);
     else newList = list;
-  
-    currentMap.set(parentId, newList);
-    return new Map(currentMap);
+
+    const newMap = new Map(currentMap);
+    newMap.set(parentId, newList);
+    return newMap;
 };
 
 export {
