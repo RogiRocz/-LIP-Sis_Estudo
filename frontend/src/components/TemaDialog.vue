725cf1de74e1a3f4182b371c02373a0e01c27829
rogirocz2 <rogirocz@gmail.com>
2026-01-07 05:40:00 +0000

    feat: continuação da implementação do crud de tema

--- frontend/src/components/TemaDialog.vue
+++ frontend/src/components/TemaDialog.vue
@@ -1,31 +1,30 @@
 <script setup lang="ts">
+import { createTema, updateTema } from '@/api/tema'
 import { useAppBarStore } from '@/stores/useAppBarStore'
 import { useSnackbarStore } from '@/stores/useSnackbarStore'
 import { useUserStore } from '@/stores/useUserStore'
+import { Revisao } from '@/utils/apiTypes'
 import { required } from '@/utils/rulesAuth'
 import { storeToRefs } from 'pinia'
-import { computed, onMounted, ref, watch } from 'vue'
+import { ref, watch } from 'vue'
 
 const dialog = ref(false)
 const loading = ref(false)
 const form = ref()
-const nome = ref('')
-const descricao = ref('')
-const revisoes = ref<string[]>([])
+const nome = ref('Um tema qualquer')
+const descricao = ref('Descricao boa')
 const id = ref(null)
+const id_disciplina = ref(null)
 
 const snackbarStore = useSnackbarStore()
 const { addMessage } = snackbarStore
 
 const appBarStore = useAppBarStore()
-const { nameDisplay } = storeToRefs(appBarStore)
+const { actionsWidthDialog } = storeToRefs(appBarStore)
 
 const { getUser } = useUserStore()
 const { intervalo_revisoes } = getUser
-
-const actionsWidthDialog = computed(() => {
-	return nameDisplay.value === 'xs' ? '100%' : '50vw'
-})
+const intervalo = ref<number[]>(intervalo_revisoes)
 
 const props = defineProps<{
 	title: string
@@ -34,25 +33,93 @@ const props = defineProps<{
 	initialData?: any
 }>()
 
+function calcDays(r: Revisao) {
+	const dataRev = new Date(r.data_prevista).getTime()
+	const hoje = new Date().getTime()
+	const diff = dataRev - hoje
+	const days = Math.ceil(diff / (1000 * 60 * 60 * 24))
+	return days
+}
+
 watch(
 	() => dialog.value,
 	(isOpen) => {
+		if(!isOpen) return
+
 		if (isOpen && props.whichFuncToCall === 'update' && props.initialData) {
 			nome.value = props.initialData.nome
 			descricao.value = props.initialData.descricao
-			revisoes.value = props.initialData.revisoes
 			id.value = props.initialData.id || props.initialData.ID
+			id_disciplina.value = props.initialData.id_disciplina
+
+			const revisoes: Revisao[] = props.initialData.revisoes
+			if (revisoes && revisoes.length > 0) {
+				intervalo.value = revisoes
+					.filter((r) => r.status !== 'REALIZADA')
+					.map((r) => calcDays(r))
+			}
+		} else {
+			id_disciplina.value = props.initialData.id_disciplina
+			nome.value = ''
+            descricao.value = ''
 		}
 	},
 )
 
 async function handleClick() {
-	console.log('Entrou')
+	try {
+		loading.value = true
+
+		const { valid } = await form.value.validate()
+
+		if (!valid) {
+			addMessage({ text: 'Preencha todos os campos', color: 'error' })
+			return
+		}
+
+		const payload = {
+			nome: nome.value,
+			descricao: descricao.value,
+			disciplina_id: id_disciplina.value,
+			intervalos: intervalo.value,
+		}		
+
+		if (props.whichFuncToCall === 'create') {			
+			const response = await createTema(id_disciplina.value, payload)
+			addMessage({ text: 'Tema criado com sucesso', color: 'success' })
+		} else if (props.whichFuncToCall === 'update') {
+			const response = await updateTema(id.value!, payload)
+			addMessage({ text: 'Tema alterado com sucesso', color: 'success' })
+		}
+
+		nome.value = ''
+		descricao.value = ''
+		// intervalo.value = null
+		form.value.reset()
+		dialog.value = false
+	} catch (error) {
+		if (props.whichFuncToCall === 'create') {
+			console.log('Erro ao criar o tema: ', error)
+			addMessage({ text: 'Erro ao criar o tema', color: 'error' })
+		} else if (props.whichFuncToCall === 'update') {
+			console.log('Erro ao atualizar o tema: ', error)
+			addMessage({ text: 'Erro ao atualizar o tema', color: 'error' })
+		}
+	} finally {
+		loading.value = false
+	}
 }
 
-onMounted(() => {
-	console.log(intervalo_revisoes.split(','))
-})
+function addInterval() {
+	const ultimoValor =
+		intervalo.value.length > 0 ? intervalo.value[intervalo.value.length - 1] : 0
+
+	intervalo.value.push(ultimoValor + 7)
+}
+
+function removeInterval(index: number) {
+	intervalo.value.splice(index, 1)
+}
 </script>
 
 <template>
@@ -84,7 +151,7 @@ onMounted(() => {
 						>Defina quando você quer revisar este tema após estudá-lo</span
 					>
 					<v-container
-						v-for="(revisao, i) in intervalo_revisoes.split(',')"
+						v-for="(revisao, i) in intervalo"
 						class="revisao-container"
 					>
 						<v-row :key="i" class="revisao-container">
@@ -93,7 +160,7 @@ onMounted(() => {
 								class="custom-number-input"
 								control-variant="stacked"
 								max-width="10vw"
-								:model-value="Number(revisao)"~
+								:model-value="Number(revisao)"
 								variant="outlined"
 								inset
 								density="compact"
@@ -103,7 +170,7 @@ onMounted(() => {
 							<v-btn
 								variant="plain"
 								icon="close"
-								@click="revisoes.splice(i, 1)"
+								@click="removeInterval(i)"
 								color="error"
 								density="compact"
 								elevation="elevation-10"
@@ -112,10 +179,13 @@ onMounted(() => {
 						</v-row>
 					</v-container>
 					<v-btn
-					variant="outlined"
-					class="revisao-add"
-					color="primary" 
-					 prepend-icon="add">Adicionar intervalo</v-btn>
+						variant="outlined"
+						class="revisao-add"
+						color="primary"
+						prepend-icon="add"
+						@click.prevent="addInterval"
+						>Adicionar intervalo</v-btn
+					>
 				</v-form>
 			</v-card-text>
 			<v-card-actions>
@@ -149,7 +219,7 @@ onMounted(() => {
 .input-subtitle {
 	color: #767676d6;
 	font-size: 0.875rem;
-    margin-bottom: 8px;
+	margin-bottom: 8px;
 	font-weight: 300;
 }
 
@@ -163,19 +233,19 @@ onMounted(() => {
 }
 
 .custom-number-input {
-    width: 10vw !important;
-    flex: none !important;
-    min-width: 0 !important;
+	width: 10vw !important;
+	flex: none !important;
+	min-width: 0 !important;
 }
 
 .custom-number-input :deep(input) {
-    text-align: center !important;
-    padding: 0;
+	text-align: center !important;
+	padding: 0;
 	height: 100%;
 }
 
 .custom-number-input :deep(.v-input__control) {
-    min-width: 0 !important;
-    width: 100% !important;
+	min-width: 0 !important;
+	width: 100% !important;
 }
 </style>
