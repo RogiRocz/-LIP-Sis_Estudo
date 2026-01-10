76d3be679461200dab3b5a1cff46e61b9a61b678
rogirocz2 <rogirocz@gmail.com>
2026-01-10 20:35:51 +0000

    feat: adicionando lógica e funcionalidade a rota

--- backend/src/routers/painel.py
+++ backend/src/routers/painel.py
@@ -1,18 +1,32 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from ..database import get_db
-from .. import models
+from ..models.user import User as UserModel
 from ..security import get_current_user
 from ..schemas import painel as painel_schema
 from ..services.painel import PainelService
+from typing import List
 
 router = APIRouter(prefix="/painel", tags=["Painel"])
 
 
-@router.get("/estatisticas", response_model=painel_schema.PainelData)
-def get_painel_data(
-    db: Session = Depends(get_db),
-    user: models.user.User = Depends(get_current_user),
+@router.get("/estatisticas", response_model=painel_schema.EstatisticasGerais)
+async def get_estatisticas(
+    user: UserModel = Depends(get_current_user),
+    service: PainelService = Depends(),
 ):
-    painel_service = PainelService(db)
-    return painel_service.get_painel_data(user.id_usuario)
+    return await service.get_estatisticas_gerais(user.ID)
+
+
+@router.get("/evolucao-semanal", response_model=List[painel_schema.EvolucaoSemanal])
+async def get_evolucao_semanal(
+    user: UserModel = Depends(get_current_user),
+    service: PainelService = Depends(),
+):
+    return await service.get_evolucao_semanal(user.ID)
+
+
+@router.get("/revisoes-do-dia", response_model=List[painel_schema.RevisaoDoDia])
+async def get_revisoes_do_dia(
+    user: UserModel = Depends(get_current_user),
+    service: PainelService = Depends(),
+):
+    return await service.get_revisoes_do_dia(user.ID)
