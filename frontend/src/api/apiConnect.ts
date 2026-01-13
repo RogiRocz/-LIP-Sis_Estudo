41dd52e1743675d1fe2223f97683a5f91cca3feb
rogirocz2 <rogirocz@gmail.com>
2026-01-13 19:40:13 +0000

    feat: aplicando função para dar um refresh no token se puder

--- frontend/src/api/apiConnect.ts
+++ frontend/src/api/apiConnect.ts
@@ -2,6 +2,8 @@ import axios from 'axios'
 import router from '@/router'
 import { useUserStore } from '@/stores/useUserStore'
 import { useSnackbarStore } from '@/stores/useSnackbarStore'
+import { isTokenExpired, shouldRefreshToken } from '@/utils/tokens'
+import { refresh_token } from './auth'
 
 const api = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
@@ -19,7 +21,7 @@ api.interceptors.request.use(
 			config.headers.set('Authorization', `Bearer ${token}`)
 		}
 
-    // Gambiarra para funcionar no ambiente virtual do google workstations que não suporta o método PUT
+		// Gambiarra para funcionar no ambiente virtual do google workstations que não suporta o método PUT
 		if (config.method === 'put') {
 			config.method = 'post'
 			config.headers.set('X-HTTP-Method-Override', 'PUT')
@@ -34,22 +36,37 @@ api.interceptors.request.use(
 
 api.interceptors.response.use(
 	(response) => response,
-
 	async (error) => {
 		const userStore = useUserStore()
 		const snackbarStore = useSnackbarStore()
+		const originalRequest = error.config
 
-		if (error.response && error.response.status === 401) {
-			userStore.logout()
+		const isLoginRequest = originalRequest.url.includes('/auth/login')
 
-			snackbarStore.addMessage({
-				text: 'Sua sessão expirou. Por favor, faça login novamente.',
-				color: 'warning',
-			})
+		if (error.response?.status === 401 && !originalRequest._retry && !isLoginRequest) {
+			originalRequest._retry = true
 
-			await router.push({ name: 'Login' })
+			try {
+				const data = await refresh_token(userStore.refresh_token)
+
+				userStore.setTokens(
+					data.token,
+					data.refresh_token,
+					data.expires_at,
+				)
+
+				originalRequest.headers['Authorization'] = `Bearer ${data.token}`
+				return api(originalRequest)
+			} catch (refreshError) {
+				userStore.logout()
+				snackbarStore.addMessage({
+					text: 'Sua sessão expirou. Faça login novamente.',
+					color: 'warning',
+				})
+				router.push({ name: 'Login' })
+				return Promise.reject(refreshError)
+			}
 		}
-
 		return Promise.reject(error)
 	},
 )
