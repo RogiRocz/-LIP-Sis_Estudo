e77dd1397602b4856b68b0bd4106fa1a2a7d8ab6
rogirocz2 <rogirocz@gmail.com>
2026-01-15 01:32:30 +0000

    feat: adicionando rota de logout e ajustando a de refresh

--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends
-from ..schemas.user import UserCreate, UserLogin
-from ..schemas.auth import Token, RefreshTokenResponse, RefreshTokenRequest
+from fastapi import APIRouter, Depends, Response, status, HTTPException
+from ..schemas.user import UserCreate, UserLogin, User
+from ..schemas.auth import Token, RefreshTokenResponse, RefreshTokenRequest, TokenLogout
 from ..services.auth import AuthService
+from ..security import get_current_user
+from ..core.supabase_client import supabase
+
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -9,12 +12,23 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 async def register(user: UserCreate, service: AuthService = Depends()):
     return await service.register(user)
 
+
 @router.post("/login", response_model=Token)
 async def login(user: UserLogin, service: AuthService = Depends()):
     return await service.login(user)
 
+
 @router.post("/refresh", response_model=RefreshTokenResponse)
-async def refresh_token(
-    request: RefreshTokenRequest, service: AuthService = Depends()
-):
-    return await service.refresh(request.refresh_token)
+async def refresh_token(user: UserLogin, service: AuthService = Depends()):
+    return await service.refresh(user)
+
+
+@router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
+async def logout(token: TokenLogout, service: AuthService = Depends()):
+    try:
+        await service.logout(token.token)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
