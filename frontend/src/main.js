fab939aa8afdf0cfeb032133451a3a6ca65c3c46
rogirocz2 <rogirocz@gmail.com>
2026-01-22 01:08:21 +0000

    feat: adicionando lógica para a persistencia das stores

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,19 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 import vuetify from './config/vuetify'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(vuetify)
-app.use(router)
-app.mount('#app')
+const initApp = async () => {
+	const app = createApp(App)
+	const pinia = createPinia()
+	pinia.use(piniaPluginPersistedstate)
+
+	app.use(vuetify)
+	app.use(pinia)
+	app.use(router)
+	await router.isReady()
+
+	app.mount('#app')
+}
+
+initApp()
