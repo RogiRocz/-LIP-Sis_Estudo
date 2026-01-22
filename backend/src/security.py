c62849bc183809902a1f73256f1d44fab5f355a7
rogirocz2 <rogirocz@gmail.com>
2026-01-22 01:01:23 +0000

    fix: ajustes para usar as claims oficial

--- backend/src/security.py
+++ backend/src/security.py
@@ -10,6 +10,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, or_
 from .database import get_db
 import uuid
+import json
 
 
 security_scheme = HTTPBearer()
@@ -28,7 +29,10 @@ def get_password_hash(password):
 def create_token(
     user_data: dict,
     access_token_expires_minutes: Optional[int] = None,
+    token_headers: Optional[dict] = None,
 ) -> Tuple[str, datetime]:
+
+    payload = user_data.get('claims', {})
 
     if access_token_expires_minutes is None:
         access_token_expires_minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES
@@ -36,86 +40,91 @@ def create_token(
     access_token_expires = timedelta(minutes=access_token_expires_minutes)
     access_token_expires_at = datetime.now(timezone.utc) + access_token_expires
 
-    access_token_data = user_data.copy()
-    access_token_data.update(
+    payload.update(
         {
             "exp": int(access_token_expires_at.timestamp()),
-            "token_type": "access",
-            "aud": "authenticated",
-            "role": "authenticated",
         }
     )
 
+    user_data['claims'] = payload
+    access_token_data = user_data
+
+    secret_key = settings.SUPABASE_SECRET_KEY
+    if isinstance(secret_key, str):
+        secret_key = secret_key.encode('utf-8')
+
     access_token = jwt.encode(
-        access_token_data, settings.SUPABASE_JWT_SECRET, algorithm="HS256"
+        access_token_data,
+        secret_key,
+        algorithm="HS256",
+        headers=token_headers,
     )
 
-    
     return access_token, access_token_expires_at
-
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-
-    if "aud" not in to_encode:
-        to_encode.update({"aud": "authenticated"})
-
-    if "role" not in to_encode:
-        to_encode.update({"role": "authenticated"})
-
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SUPABASE_JWT_SECRET, algorithm="HS256")
-    return encoded_jwt
 
 
 def verify_token(token: str, token_type: str = "access") -> dict:
     try:
-        payload = jwt.decode(
-            token,
-            settings.SUPABASE_JWT_SECRET,
-            algorithms=["HS256"],
-            audience="authenticated",
+        public_key = json.loads(settings.SUPABASE_PUBLIC_KEY)
+
+        return jwt.decode(
+            token, public_key, algorithms=["ES256"], audience="authenticated"
         )
+    except Exception:
+        try:
+            # 2. Fallback: Tenta validar com o Secret (Para tokens HS256)
+            return jwt.decode(
+                token,
+                settings.SUPABASE_SECRET_KEY,
+                algorithms=["HS256"],
+                audience="authenticated",
+            )
+        except JWTError as e:
+            raise HTTPException(status_code=401, detail="Token inválido ou expirado")
 
-        if payload.get("token_type") != token_type:
-            raise JWTError("Tipo de token inválido")
 
-        return payload
-
-    except JWTError as e:
-        raise e
-
-
-def refresh_access_token(user_data: User, supabase_user: dict) -> Tuple[str, datetime]:
+def refresh_access_token(
+    user_data: User, supabase_user: dict, token_headers: Optional[dict] = None
+) -> Tuple[str, datetime]:
     try:
         supabase_id = user_data.supabase_id
 
         access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
         access_token_expires_at = datetime.now(timezone.utc) + access_token_expires
 
+        # claims = {
+        #     "iss": f"{settings.SUPABASE_URL}/auth/v1",
+        #     "session_id": str(uuid.uuid4()),
+        #     "sub": str(supabase_id),
+        #     "email": user_data.email,
+        #     "phone": supabase_user.phone or "",
+        #     "app_metadata": {"provider": "email", "providers": ["email"]},
+        #     "user_metadata": {"nome": user_data.nome, "email_verified": True},
+        #     "aal": "aal1",
+        #     "amr": [
+        #         {
+        #             "method": "password",
+        #             "timestamp": int(datetime.now(timezone.utc).timestamp()),
+        #         }
+        #     ],
+        #     "iat": int(datetime.now(timezone.utc).timestamp()),
+        #     "role": "authenticated",
+        #     "aud": "authenticated",
+        #     "exp": int(access_token_expires_at.timestamp()),
+        #     "is_anonymous": supabase_user.is_anonymous or False,
+        # }
+
         token_data = {
-            "iss": f"{settings.SUPABASE_URL}/auth/v1",
-            "session_id": str(uuid.uuid4()),
-            "sub": supabase_id,
-            "email": user_data.email,
-            "nome": user_data.nome,
-            "phone": supabase_user.phone | "",
-            "aal": "aal1",
-            "iat": int(datetime.now(timezone.utc).timestamp()),
-            "role": "authenticated",
-            "aud": "authenticated",
-            "exp": int(access_token_expires_at.timestamp()),
-            "is_anonymous": supabase_user.is_anonymous | False,
+            "user_id": str(supabase_id),
+            "claims": claims,
+            "authentication_method": "password",
         }
 
         access_token = jwt.encode(
-            token_data, settings.SUPABASE_JWT_SECRET, algorithm="HS256"
+            token_data,
+            settings.SUPABASE_SECRET_KEY,
+            algorithm="HS256",
+            headers=token_headers,
         )
 
         return access_token, access_token_expires_at
@@ -137,15 +146,11 @@ async def get_current_user(
     try:
         token = credentials.credentials
 
-        payload = jwt.decode(
-            token,
-            settings.SUPABASE_JWT_SECRET,
-            algorithms=["HS256"],
-            audience="authenticated",
-        )
+        payload = verify_token(token)
+        payload = payload.get("claims", {})
 
         supabase_id_str = payload.get("sub")
-        email = payload.get("email")
+        email = payload.get("email") or payload.get("user_metadata", {}).get("email")
 
         if not supabase_id_str or supabase_id_str == "None":
             raise credentials_exception
